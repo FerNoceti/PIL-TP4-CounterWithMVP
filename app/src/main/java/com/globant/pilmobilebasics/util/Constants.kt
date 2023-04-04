089a78931d97205ba1e376c41f403e070b5bb33f
Fernando José Noceti <fer.j.noceti@gmail.com>
2023-04-04 01:17:34 -0300

    MainPresenterTest

--- app/src/main/java/com/globant/pilmobilebasics/util/Constants.kt
+++ app/src/main/java/com/globant/pilmobilebasics/util/Constants.kt
@@ -2,5 +2,9 @@ package com.globant.pilmobilebasics.util
 
 object Constants {
     const val ZERO = 0
+
     const val MessageInputError = "Please enter a number"
+
+    const val testInput = "10"
+    const val minusTestInput = "-10"
 }
--- app/src/test/java/com/globant/pilmobilebasics/mvp/presenter/MainPresenterTest.kt
+++ app/src/test/java/com/globant/pilmobilebasics/mvp/presenter/MainPresenterTest.kt
@@ -1,31 +1,38 @@
 package com.globant.pilmobilebasics.mvp.presenter
 
 import com.globant.pilmobilebasics.mvp.contract.MainContract
+import com.globant.pilmobilebasics.mvp.model.MainModel
+import com.globant.pilmobilebasics.util.Constants.ZERO
+import com.globant.pilmobilebasics.util.Constants.minusTestInput
+import com.globant.pilmobilebasics.util.Constants.testInput
+import io.mockk.MockKAnnotations
 import io.mockk.every
-import io.mockk.mockk
+import io.mockk.impl.annotations.MockK
 import io.mockk.verify
 import org.junit.Before
 import org.junit.Test
 
 class MainPresenterTest {
 
-    private lateinit var presenter: MainContract.Presenter
-    private lateinit var view: MainContract.View
     private lateinit var model: MainContract.Model
+
+    @MockK
+    private lateinit var view: MainContract.View
+
+    private lateinit var presenter: MainContract.Presenter
 
     @Before
     fun setUp() {
-        view = mockk(relaxed = true)
-        model = mockk(relaxed = true)
+        MockKAnnotations.init(this, relaxed = true)
+        model = MainModel()
         presenter = MainPresenter(model, view)
     }
 
     @Test
     fun `when increment button is pressed and input is valid, model is called and view is updated`() {
         // Given
-        val input = "2"
         every { view.checkNumber() } returns true
-        every { view.getNumber() } returns input
+        every { view.getNumber() } returns testInput
 
         // When
         presenter.onIncrementButtonPressed()
@@ -33,16 +40,15 @@ class MainPresenterTest {
         // Then
         verify { view.checkNumber() }
         verify { view.getNumber() }
-        verify { model.increment(input) }
-        verify { view.setCount(any()) }
+        verify { view.setCount(testInput) }
+        assert(model.getCount() == testInput)
     }
 
     @Test
     fun `when decrement button is pressed and input is valid, model is called and view is updated`() {
         // Given
-        val input = "2"
         every { view.checkNumber() } returns true
-        every { view.getNumber() } returns input
+        every { view.getNumber() } returns testInput
 
         // When
         presenter.onDecrementButtonPressed()
@@ -50,18 +56,21 @@ class MainPresenterTest {
         // Then
         verify { view.checkNumber() }
         verify { view.getNumber() }
-        verify { model.decrement(input) }
-        verify { view.setCount(any()) }
+        verify { view.setCount(minusTestInput) }
+        assert(model.getCount() == minusTestInput)
     }
 
     @Test
     fun `when reset button is pressed, model is called and view is updated`() {
+        // Given
+        model.increment(testInput)
+
         // When
         presenter.onResetButtonPressed()
 
         // Then
-        verify { model.reset() }
-        verify { view.setCount(any()) }
+        verify { view.setCount(ZERO.toString()) }
+        assert(model.getCount() == ZERO.toString())
     }
 
     @Test
@@ -75,8 +84,6 @@ class MainPresenterTest {
         // Then
         verify { view.checkNumber() }
         verify { view.showNumberError() }
-        verify(exactly = 0) { model.increment(any()) }
-        verify(exactly = 0) { view.setCount(any()) }
     }
 
     @Test
@@ -90,7 +97,5 @@ class MainPresenterTest {
         // Then
         verify { view.checkNumber() }
         verify { view.showNumberError() }
-        verify(exactly = 0) { model.decrement(any()) }
-        verify(exactly = 0) { view.setCount(any()) }
     }
 }
